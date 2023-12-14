47ceabfd7bfb591cb81d070a6b328fc5fa956a53
34n0 <34n0@immerda.ch>
2023-12-14 23:34:43 +0100

    :recycle: Mv options to settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,48 +1,38 @@
-extern crate ini;
-extern crate lazy_static;
-extern crate serde;
-extern crate users;
-extern crate once_cell;
-
-
+use crate::Actions;
+use ini::Ini;
+use once_cell::sync::Lazy;
+use pam::constants::PamFlag;
 use std::collections::HashMap;
 use std::env;
 use std::ffi::CStr;
-use std::path::{Path, PathBuf};
-
-use self::ini::Ini;
-use self::lazy_static::lazy_static;
-use self::serde::Deserialize;
-use self::users::{get_user_by_name, User};
-use crate::Actions;
-use self::once_cell::sync::Lazy;
-use pam::constants::PamResultCode;
-use pam::{constants::PamFlag, module::PamResult};
+use std::path::PathBuf;
+use users::{get_user_by_name, User};
 
 const DEFAULT_TALLY_DIR: &str = "/var/run/rampdelay";
 const DEFAULT_CONFIG_PATH: &str = "/etc/security/rampdelay.conf";
 static CONFIG_PATH: Lazy<String> = Lazy::new(|| {
-    env::var("TEST_CONFIG_PATH").unwrap_or_else(|_| DEFAULT_CONFIG_PATH.to_string())
+    env::var("TEST_CONFIG_PATH").unwrap_or_else(|_| String::from(DEFAULT_CONFIG_PATH))
 });
 
 #[derive(Debug)]
-pub struct Options {
-     pub action: Option<Actions>,
-    // pub user: User,
+pub struct Settings {
+    pub action: Option<Actions>,
+    pub user: Option<User>,
     pub tally_dir: String,
 }
 
-impl Default for Options {
+impl Default for Settings {
     fn default() -> Self {
-        Options {
+        Settings {
             tally_dir: String::from(DEFAULT_TALLY_DIR),
             action: None,
+            user: None,
         }
     }
 }
 
-impl Options {
-    pub fn build(_user: String, args: Vec<&CStr>, _flags: PamFlag) -> Options {
+impl Settings {
+    pub fn build(username: String, args: Vec<&CStr>, _flags: PamFlag) -> Settings {
         // Load INI file.
         let mut opts = Ini::load_from_file(PathBuf::from(CONFIG_PATH.to_string()).as_path())
             .ok()
@@ -51,26 +41,36 @@ impl Options {
                 ini.section(Some("Settings"))
                     .map(|settings| settings.clone())
             })
-            // Map section to Options struct, defaulting "tally_dir" if absent.
-            .map(|settings| Options {
+            // Map section to Settings struct, defaulting "tally_dir" if absent.
+            .map(|settings| Settings {
                 tally_dir: settings
                     .get("tally_dir")
                     .map(|s| s.to_string())
                     .unwrap_or_default(),
-                ..Options::default()
+                ..Settings::default()
             })
-            // Fallback to default Options if any failures.
-            .unwrap_or_else(|| Options::default());
+            // Fallback to default Settings if any failures.
+            .unwrap_or_else(|| Settings::default());
 
-            let action_map: HashMap<&str, Actions> = [
-                ("preauth", Actions::PREAUTH),
-                ("authsucc", Actions::AUTHSUCC),
-                ("authfail", Actions::AUTHFAIL),
-            ].iter().copied().collect();
-            
-            opts.action = args
-                .iter()
-                .find_map(|&carg| carg.to_str().ok().and_then(|arg| action_map.get(arg).cloned()));
+        // create possible action collection
+        let action_map: HashMap<&str, Actions> = [
+            ("preauth", Actions::PREAUTH),
+            ("authsucc", Actions::AUTHSUCC),
+            ("authfail", Actions::AUTHFAIL),
+        ]
+        .iter()
+        .copied()
+        .collect();
+
+        // map argument to action
+        opts.action = args.iter().find_map(|&carg| {
+            carg.to_str()
+                .ok()
+                .and_then(|arg| action_map.get(arg).cloned())
+        });
+
+        // get user
+        opts.user = get_user_by_name(&username);
 
         opts
     }
@@ -106,7 +106,7 @@ mod tests {
         set_test_conf_path();
         let args = [].to_vec();
         let flags: PamFlag = 0;
-        let opts = Options::build(USER_NAME.to_string(), args, flags);
+        let opts = Settings::build(USER_NAME.to_string(), args, flags);
         assert_eq!(
             opts.tally_dir, "./tests/tally",
             "Expected ./tests/tally tall_dir value"
@@ -119,7 +119,7 @@ mod tests {
         set_test_conf_path();
         let args = [].to_vec();
         let flags: PamFlag = 0;
-        let opts = Options::build(USER_NAME.to_string(), args, flags);
+        let opts = Settings::build(USER_NAME.to_string(), args, flags);
         assert!(opts.action.is_none(), "Expected action to be None");
         Ok(())
     }
@@ -129,52 +129,50 @@ mod tests {
         set_test_conf_path();
         let args = [CStr::from_bytes_with_nul("preauth\0".as_bytes())?].to_vec();
         let flags: PamFlag = 0;
-        let opts = Options::build(USER_NAME.to_string(), args, flags);
-        assert_eq!(opts.action, Some(Actions::PREAUTH), "Expected action to be Preauth");
+        let opts = Settings::build(USER_NAME.to_string(), args, flags);
+        assert_eq!(
+            opts.action,
+            Some(Actions::PREAUTH),
+            "Expected action to be Preauth"
+        );
         Ok(())
-    }  
-    
+    }
+
     #[test]
     fn test_action_authfail() -> TestResult {
         set_test_conf_path();
         let args = [CStr::from_bytes_with_nul("authfail\0".as_bytes())?].to_vec();
         let flags: PamFlag = 0;
-        let opts = Options::build(USER_NAME.to_string(), args, flags);
-        assert_eq!(opts.action, Some(Actions::AUTHFAIL), "Expected action to be Authfail");
+        let opts = Settings::build(USER_NAME.to_string(), args, flags);
+        assert_eq!(
+            opts.action,
+            Some(Actions::AUTHFAIL),
+            "Expected action to be Authfail"
+        );
         Ok(())
     }
-    /*
+
     #[test]
     fn test_conf_load_uname_valid() -> TestResult {
-      format  set_test_env();
+        set_test_conf_path();
 
         let args: Vec<&CStr> = [].to_vec();
         let flags: PamFlag = 0;
 
-        let opts = Options::build(USER_NAME.to_string(), args, flags)
-            .expect("failed loading config with valid user");
-        assert_eq!(opts.user.name(), USER_NAME);
-
+        let opts = Settings::build(USER_NAME.to_string(), args, flags);
+        assert!(opts.user.is_some(), "Expected user to be Some");
         Ok(())
     }
 
     #[test]
     fn test_conf_load_uname_invalid() -> TestResult {
-        set_test_env();
+        set_test_conf_path();
 
         let args: Vec<&CStr> = [].to_vec();
         let flags: PamFlag = 0;
 
-
-        let res = Options::build("invalid".to_string(), args, flags)
-            .expect_err("success loading config with invalid username!");
-        assert_eq!(
-            res,
-            PamResultCode::PAM_SYSTEM_ERR,
-            "Expected PAM_SYSTEM_ERR"
-        );
-
+        let opts = Settings::build("invalid".to_string(), args, flags);
+        assert!(opts.user.is_none(), "Expected user to be none");
         Ok(())
     }
-    */
 }
