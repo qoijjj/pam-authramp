6537ab87459df2aca8c469eb70ce9cc48b6b9831
34n0 <34n0@immerda.ch>
2023-12-20 19:11:02 +0100

    test: consecutive failures  creates tally

--- tests/common/utils.rs
+++ tests/common/utils.rs
@@ -1,3 +1,4 @@
+use std::fs::{remove_dir, remove_dir_all};
 use std::io;
 use std::path::PathBuf;
 
@@ -6,10 +7,11 @@ use std::{
     io::Write,
 };
 
+use pam_client::conv_mock::Conversation;
+use pam_client::Context;
+
 pub const SRV_DIR: &str = "/etc/pam.d";
 pub const PAM_SRV: &str = "test-rampdelay";
-
-const USER_NAME: &str = dotenv!("TEST_USER_NAME");
 
 fn create_pam_service_file() -> io::Result<()> {
     let mut file = File::create(PathBuf::from(SRV_DIR).join(PAM_SRV))?;
@@ -28,15 +30,26 @@ fn remove_pam_service_file() -> io::Result<()> {
     Ok(())
 }
 
-pub fn create_and_remove_pam_service<F>(test: F)
+fn clear_tally_dir() -> Result<(), io::Error> {
+    remove_dir_all("/var/run/rampdelay")?;
+    Ok(())
+}
+
+pub fn init_and_clear_test<F>(test: F)
 where
     F: FnOnce(),
 {
     create_pam_service_file().expect("Failed to create PAM service file");
     test();
     remove_pam_service_file().expect("Failed to remove PAM service file");
+    clear_tally_dir().expect("Failes clearing tally dir");
 }
 
-pub fn get_tally_file_path() -> PathBuf {
-    PathBuf::from("/var/run/rampdelay").join(USER_NAME)
+pub fn get_pam_context(u_name: &str, u_pwd: &str) -> Context<Conversation> {
+    Context::new(PAM_SRV, None, Conversation::with_credentials(u_name, u_pwd))
+        .expect("Failed creating PAM context!")
+}
+
+pub fn get_tally_file_path(u_name: &str) -> PathBuf {
+    PathBuf::from("/var/run/rampdelay").join(u_name)
 }
--- tests/test-pam-auth.rs
+++ tests/test-pam-auth.rs
@@ -5,22 +5,22 @@ mod common;
 
 #[cfg(test)]
 mod test_pam_auth {
+    use std::fs;
+
+    use crate::common::utils::get_pam_context;
+
     use super::common::utils;
-    use pam_client::{conv_mock::Conversation, Context, Flag};
+    use pam_client::Flag;
 
     const USER_NAME: &str = dotenv!("TEST_USER_NAME");
     const USER_PWD: &str = dotenv!("TEST_USER_PWD");
 
     #[test]
     fn test_valid_auth_success() {
-        utils::create_and_remove_pam_service(|| {
-            let mut ctx = Context::new(
-                utils::PAM_SRV, // Service name
-                None,
-                Conversation::with_credentials(USER_NAME, USER_PWD),
-            )
-            .expect("Failed to create PAM context");
+        utils::init_and_clear_test(|| {
+            let mut ctx = get_pam_context(USER_NAME, USER_PWD);
 
+            // Expect the authentication to succeed
             ctx.authenticate(Flag::NONE).expect("Authentication failed");
             ctx.acct_mgmt(Flag::NONE)
                 .expect("Account management failed")
@@ -29,21 +29,41 @@ mod test_pam_auth {
 
     #[test]
     fn test_invalid_auth_creates_tally() {
-        utils::create_and_remove_pam_service(|| {
-            let mut ctx = Context::new(
-                utils::PAM_SRV, // Service name
-                None,
-                Conversation::with_credentials("INVALID", "CREDENTIALS"),
-            )
-            .expect("Failed to create PAM context");
+        utils::init_and_clear_test(|| {
+            let mut ctx = get_pam_context(USER_NAME, "INVALID");
 
             // Expect an error during authentication (invalid credentials)
             let auth_result = ctx.authenticate(Flag::NONE);
             assert!(auth_result.is_err(), "Authentication succeeded!");
 
             // Expect tally file gets created
-            let tally_file_path = utils::get_tally_file_path();
+            let tally_file_path = utils::get_tally_file_path(USER_NAME);
             assert!(tally_file_path.exists(), "Tally file not created")
         });
+    }
+
+    #[test]
+    fn test_consecutive_invalid_adds_tally() {
+        utils::init_and_clear_test(|| {
+            let mut ctx = get_pam_context(USER_NAME, "INVALID");
+
+            let mut count = 0;
+
+            while count < 3 {
+                // Expect an error during authentication (invalid credentials)
+                let auth_result = ctx.authenticate(Flag::NONE);
+                assert!(auth_result.is_err(), "Authentication succeeded!");
+
+                count += 1;
+            }
+
+            // Expect tally file gets created
+            let tally_file_path = utils::get_tally_file_path(USER_NAME);
+            assert!(tally_file_path.exists(), "Tally file not created");
+
+            // Expect tally count
+            let ini_content = fs::read_to_string(tally_file_path).unwrap();
+            assert!(ini_content.contains("count=3"));
+        })
     }
 }
